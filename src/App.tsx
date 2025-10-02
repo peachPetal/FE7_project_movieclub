5aac4766098e86488aaa966aea9b150bb3c7a085
SangYoonLee1231 <sylagape1231@gmail.com>
2025-10-02 15:17:56 +0900

    ✨ MoviesRendering 1차 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import MoviesRendering from "./components/MoviesRendering";
+
 export default function App() {
   return (
     <>
       <div className="bg-background-sub p-4">
         <p className="text-main">메인 텍스트</p>
       </div>
+      <MoviesRendering variant="home" limit={2} />
     </>
   );
 }
