fd37e7ee9dc627371f63bbcd4be72f07957fd182
peachPetal <toneba3296@gmail.com>
2025-10-02 15:14:04 +0900

    Update DefaultLayout.tsx

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,22 +1,13 @@
-import type { ReactNode } from "react";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 
-type DefaultLayoutProps = {
-  children: ReactNode;
-};
-
-export default function DefaultLayout({ children }: DefaultLayoutProps) {
+export default function DefaultLayout () {
   return (
-    <div className="flex flex-col h-screen bg-gray-50">
-      {/* 상단 전체 Header */}
-      <Header />
-
-      {/* Header 아래 영역: 좌측 Sidebar + 메인 콘텐츠 */}
-      <div className="flex flex-1">
+    <>
+      <div className="relative w-full mx-auto h-[1080px] bg-white">
+        <Header />
         <Sidebar />
-        <main className="flex-1 p-4 overflow-y-auto">{children}</main>
       </div>
-    </div>
+    </>
   );
 }
