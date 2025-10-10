51f8620880139fd43fb942da7f7c8a9635fdba6b
Song YoungJu <moeirs545@gmail.com>
2025-10-10 11:58:11 +0900

    🎨 홈 화면에서 드롭다운 테스트하던거 삭제

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,8 @@
 import LoginPage from "../pages/LoginPage";
-import FilterDropdown from "./common/buttons/FilterDropdown";
 
 export default function HomeContent() {
   return (
     <main className="flex justify-center items-center w-full">
-      <FilterDropdown type="Reviews" />
       <LoginPage />
     </main>
   );
