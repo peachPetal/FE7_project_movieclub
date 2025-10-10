3bfbc556ea4a42c4c14f4b8ed3ea9e6ec86c2efd
Song YoungJu <moeirs545@gmail.com>
2025-10-10 11:51:15 +0900

    ✨ 공통 컴포넌트: 필터 버튼 구현 (기능은 아직 X)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import LoginPage from "../pages/LoginPage";
+import FilterDropdown from "./common/buttons/FilterDropdown";
 
 export default function HomeContent() {
   return (
     <main className="flex justify-center items-center w-full">
+      <FilterDropdown type="Reviews" />
       <LoginPage />
     </main>
   );
---
+++ src/types/Filter.d.ts
@@ -0,0 +1,20 @@
+type FilterType =
+  | "Movies"
+  | "Reviews"
+  | "Comments"
+  | "Users"
+  | "MyPosts"
+  | "Likes";
+
+interface FilterOption {
+  value: string;
+  label: string;
+}
+/*
+Movies : 전체보기, 장르 ... 
+Reviews : 최신순, 인기순
+Comments : 최신순, 인기순
+Users : 모든 유저, 친구
+프로필 -> 내 게시물(MyPosts) : 리뷰, 댓글
+         좋아요(Likes) : 영화, 리뷰
+*/
