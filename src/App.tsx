1d0e00f36ac632c1e04b55446c2fa53d974b2f39
Song YoungJu <moeirs545@gmail.com>
2025-10-10 13:08:38 +0900

    🎨 라우터 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import LoginPage from "./pages/LoginPage";
 import Error from "./pages/Error";
 import MoviesPage from "./pages/MoviesPage";
+import HomeContent from "./pages/HomeContent";
 // import MoviesDetail from "./pages/MoviesDetail";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />} />
+    <Routes>
+      <Route element={<DefaultLayout />}>
+        {" "}
+        <Route path="/" element={<HomeContent />} />
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/error" element={<Error />} />
         <Route path="/movies" element={<MoviesPage />} />
         {/* <Route path="/movies/:id" element={<MoviesDetail />} /> */}
-      </Routes>
-    </Router>
+      </Route>
+    </Routes>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import SocialBtn from "../components/common/buttons/SocialBtn";
 export default function LoginPage() {
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full flex justify-center items-center mt-[-50px]">
         <div className="login-cotainer w-[470px] h-500px">
           <h1 className="text-4xl font-bold mb-11 text-center">로그인</h1>
           <SocialBtn socialType="google" />
