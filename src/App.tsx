dec803f84588d1f1a030a3d7215fb7a9e8f63e0b
peachPetal <toneba3296@gmail.com>
2025-10-10 09:31:26 +0900

    ⚡️ fix/404page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import LoginPage from "./pages/LoginPage";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<DefaultLayout />} />
         <Route path="/loginPage" element={<LoginPage />} />
+        <Route path="/error" element={<Error />}/>
       </Routes>
     </Router>
   );
