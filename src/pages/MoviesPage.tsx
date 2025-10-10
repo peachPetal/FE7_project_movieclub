4a517f38a2ff308724dd7e0e00169eba02d72ae6
SangYoonLee1231 <sylagape1231@gmail.com>
2025-10-10 14:13:15 +0900

    🐛 fix: 영화 페이지 오류 수정

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,19 +1,10 @@
 import MoviesList from "../components/movies/MoviesList";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
 
-export default function DefaultLayout() {
+export default function MoviesPage() {
   return (
     <>
-      <div className="w-full flex flex-col">
-        {" "}
-        <Header />{" "}
-        <div className="flex ml-[50px]">
-          <Sidebar />
-          <div className="flex min-h-[80vh] w-full">
-            <MoviesList variant="page" />
-          </div>
-        </div>
+      <div className="w-3/4 p-4 overflow-y-auto">
+        <MoviesList variant="page" />
       </div>
     </>
   );
