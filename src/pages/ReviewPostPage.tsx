0ccceff83a921d07ccc528d04ade76152099b3fd
Song YoungJu <moeirs545@gmail.com>
2025-10-14 04:22:41 +0900

    ✨ 리뷰 작성 페이지 완료

--- src/pages/ReviewPostPage.tsx
+++ src/pages/ReviewPostPage.tsx
@@ -1,12 +1,44 @@
-import { useState } from "react";
-import ReviewSearch from "../components/reviews/ReviewSearch";
+import { useEffect, useState } from "react";
 import ReviewPostBtn from "../components/reviews/ReviewPostBtn";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import { HashtagIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
 
 export default function ReviewPostPage() {
   const [title, setTitle] = useState("");
-  const [movieTitle, setMovieTitle] = useState("");
+  const [movieId, setMovieId] = useState<number | null>(null);
   const [content, setContent] = useState("");
   const [thumbnail, setThumbnail] = useState("");
+  // comobobox 관련 state들
+  // db 설계 후에 { id: number; title: string } 모두 Movie 타입으로 바꿀 것
+  const [movies, setMovies] = useState<{ id: number; title: string }[]>([]);
+  const [query, setQuery] = useState("");
+  const [selected, setSelected] = useState<{
+    id: number;
+    title: string;
+  } | null>(null);
+  const [isInputFocus, setIsInputFocus] = useState(false);
+
+  const getMovies = async () => {
+    const data = await fetch("/movies-dummy.json").then((res) => res.json());
+    setMovies(data);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const filteredMovies =
+    query === ""
+      ? movies
+      : movies.filter((movie) => {
+          return movie.title.toLowerCase().includes(query.toLowerCase());
+        });
 
   const handleThumbnailUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files && e.target.files[0];
@@ -19,7 +51,9 @@ export default function ReviewPostPage() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!title || !movieTitle || !content) {
+    console.log(title, movieId, content);
+
+    if (!title || !movieId || !content) {
       alert("값을 모두 입력해주세요.");
       return;
     }
@@ -37,12 +71,78 @@ export default function ReviewPostPage() {
             placeholder="제목을 입력하세요"
             value={title}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setContent(e.target.value)
+              setTitle(e.target.value)
             }
-            className="text-[28px] pl-3 pb-3 w-full mb-5 outline-0 text-text-sub border-b-1 border-text-light placeholder:text-text-light focus:border-main"
+            id="review-title "
+            className="text-[28px] pl-3 pb-3 w-full mb-5 outline-0 border-b-1 border-text-light placeholder:text-text-light focus:border-main"
             required
           />
-          <ReviewSearch />
+          <Combobox<{ id: number; title: string }>
+            value={selected!}
+            onChange={(value) => {
+              if (value !== null) {
+                setSelected(value);
+                setMovieId(value.id);
+              }
+            }}
+            onClose={() => setQuery("")}
+          >
+            <div
+              className={clsx(
+                "w-[268px] h-[40px] border rounded-[30px] flex justify-start items-center",
+                isInputFocus ? "border-main" : "border-text-light"
+              )}
+            >
+              <HashtagIcon
+                className={clsx(
+                  "size-4 ml-3.5",
+                  selected ? "fill-main" : "fill-text-sub"
+                )}
+              />
+              <ComboboxInput
+                placeholder="영화 제목을 입력하세요"
+                onFocus={(e) => {
+                  e.target.placeholder = "";
+                  setIsInputFocus(true);
+                }}
+                onBlur={(e) => {
+                  e.target.placeholder = "영화 제목을 입력하세요";
+                  setIsInputFocus(false);
+                }}
+                className={clsx(
+                  "movie-name border-none bg-background-main py-1.5 pl-3 text-sm/6 outline-0 placeholder:text-text-placeholder",
+                  selected ? "text-main" : " text-sub"
+                )}
+                displayValue={(movie: { id: number; title: string }) =>
+                  movie?.title
+                }
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
+              />
+            </div>
+
+            <ComboboxOptions
+              anchor="bottom"
+              transition
+              className={clsx(
+                "w-(--input-width) rounded-[20px] border border-main bg-background-main p-1 mt-1 empty:invisible",
+                "transition duration-100 ease-in data-leave:data-closed:opacity-0"
+              )}
+            >
+              {filteredMovies.map((movie) => (
+                <ComboboxOption
+                  key={movie.id}
+                  value={movie}
+                  className="group flex cursor-pointer items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-white/10"
+                >
+                  <div className="text-sm/6 text-text-sub hover:bg-main-10 w-full px-3 rounded-[inherit]">
+                    {movie.title}
+                  </div>
+                </ComboboxOption>
+              ))}
+            </ComboboxOptions>
+          </Combobox>
           <textarea
             id="content"
             rows={20}
