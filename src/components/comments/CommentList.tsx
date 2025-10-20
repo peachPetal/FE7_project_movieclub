0541b2a38f2033ccfa8c675c52f1a85f47278abf
Song YoungJu <moeirs545@gmail.com>
2025-10-20 16:10:40 +0900

    ✨ 리뷰 댓글 정렬 필터, 댓글/답글 등록 시 등록된 데이터 렌더링, 영화 태그 누르면 영화 페이지로

--- src/components/comments/CommentList.tsx
+++ src/components/comments/CommentList.tsx
@@ -1,4 +1,5 @@
 import type { UserProfile } from "../../hooks/useUserProfile";
+import CommentItemSkeleton from "../skeleton/CommentItemSkeleton";
 import CommentItem from "./CommentItem";
 
 export default function CommentList({
@@ -8,7 +9,7 @@ export default function CommentList({
   profile: UserProfile | undefined | null;
   comments: ReviewComment[] | undefined;
 }) {
-  if (!comments) return <p>로딩중...</p>;
+  if (!comments) return <CommentItemSkeleton />;
   else {
     return (
       <>
